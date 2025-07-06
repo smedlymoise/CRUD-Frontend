f5db3c4b31b8a6a6242218e843757401923fbdb3
Smedly Moise <smedly.moise@gmail.com>
2025-07-06 12:49:43 -0400

    updated css view all students

--- src/components/AllStudents.jsx
+++ src/components/AllStudents.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'; 
+import "./AllStudents.css";
 
 const AllStudents = () => {
   const [students, setStudents] = useState([]);
