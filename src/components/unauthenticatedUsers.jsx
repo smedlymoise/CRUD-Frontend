c3a1a88ba9245a6737aed2213baf7a598e2783f1
Muhammad <khawajat016@gmail.com>
2025-07-07 23:21:15 -0400

    creates login for isUnauthenicated users

---
+++ src/components/unauthenticatedUsers.jsx
@@ -0,0 +1,9 @@
+import React,{useState} from "react";
+import NavBar from "./NavBar";
+
+
+function isUnauthenticated (){
+const [user,setUser] = useState(null); //useState is the intal value because noone has login yet
+
+
+}
