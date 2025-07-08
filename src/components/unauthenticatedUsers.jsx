42347c0f268a1b7a3ad7bc7ff63d58313b302ee0
Muhammad <khawajat016@gmail.com>
2025-07-07 23:45:30 -0400

    able to check for authorize and non authorize users

--- src/components/unauthenticatedUsers.jsx
+++ src/components/unauthenticatedUsers.jsx
@@ -1,9 +1,37 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import NavBar from "./NavBar";
 
+function isUnauthenicated() {
+  const [user, setUser] = useState(null); //useState is the intal value because noone has login yet
 
-function isUnauthenticated (){
-const [user,setUser] = useState(null); //useState is the intal value because noone has login yet
+  const navBar = { user }; //creates a box
+  return;
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">home</Link>
+      </li>
+    </ul>
 
-
+    {!user && (
+      <>
+        <li>
+          <link to="login">Login</link>
+        </li>
+        <li>
+          <link to="signup">signup</link>
+        </li>
+      </>
+    )}
+    {user && (
+      <>
+        <li>
+          <link to="create">create</link>
+        </li>
+        <li>
+          <link to="logout">logout</link>
+        </li>
+      </>
+    )}
+  </nav>;
 }
